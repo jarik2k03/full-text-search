e37283144040d37e4b9ec724f644ca061f4a3ed8
Ярослав Воротников <iv121s05@mwp01.ipa.csc.sibsutis.ru>
2023-09-23 12:31:08 +0700

    formatted test

--- src/fsp/fsp.test.cxx
+++ src/fsp/fsp.test.cxx
@@ -1,9 +1,9 @@
-#include <gtest/gtest.h>
 #include "fsp/fsp.h"
+#include <gtest/gtest.h>
 
 TEST(CheckCalc, onSum) {
   fsp_calculator fspc;
   double result = fspc.summ(92.1, 4.4);
-  
+
   EXPECT_DOUBLE_EQ(result, 96.5);
 }
